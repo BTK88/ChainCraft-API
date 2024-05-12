d3a7640ada4e6bdd60b37abb8788c688d1cec19a
BTK88 <mamed.khodjaev@medieinstitutet.se>
2024-05-12 23:05:54 +0200

    refactor: Remove blockchainModel.js

---
+++ ChainCraft/models/Block.mjs
@@ -0,0 +1,11 @@
+export default class Block {
+  constructor(index, timestamp, previousHash, data) {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.previousHash = previousHash;
+    this.hash = '0';
+    this.data = data;
+    this.nonce = 0;
+    this.difficulty = +process.env.DIFFICULTY || 1;
+  }
+}
